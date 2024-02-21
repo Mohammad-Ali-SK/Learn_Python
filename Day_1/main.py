70e69691840a76b109bb275f092e5728ea45b94b
Mohammad Ali sk <mohammadalisk050@gmail.com>
2024-02-21 08:05:11 +0530

    Python_New

---
+++ Day_1/main.py
@@ -0,0 +1,36 @@
+# print('MOhammad ALI sk')
+# name = 'Mohammad ALI sk';
+# print(name)
+# Numbers -
+        # -Integers
+        # -Float
+        # -Complex
+# Booleans -
+        # -True
+        # -False
+# Strings -
+        # -Single quotes
+        # -Double quotes
+        # -Triple quotes
+        
+# Type casting / Type conversions  ---------------2
+        # -int()
+        # -float()
+        # -complex()
+        # -bool()
+        # -str()
+        # -list()
+# a = '12';
+# a = bool(a);
+# print(type(a)); 
+import pandas as pd
+
+data = {
+  'Name': ['John', 'Anna', 'Peter', 'Linda'],
+  'Age': [28, 34, 29, 32],
+  'City': ['New York', 'Paris', 'Berlin', 'London']
+}
+
+df = pd.DataFrame(data)
+
+print(df)
