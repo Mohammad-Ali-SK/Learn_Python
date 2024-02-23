25116f92d9e3b53eb874784069cbb3454208ea11
Mohammad Ali sk <mohammadalisk050@gmail.com>
2024-02-23 23:48:21 +0530

    2/23/24

---
+++ Python_Question/loop_qu.py
@@ -0,0 +1,87 @@
+# Question-----------------------------------1.0)
+
+# n = int(input("Enter number_"))
+# for i in range(0,n+1):
+#     print(i)
+
+# Question ------------------------------2.0)
+
+# n = int(input("Enter number_"))
+# for i in range(n,0,-1):
+#     print(i);
+
+# Question --------------------------------3.0)
+# num = int(input("Enter the number_"))
+# for i in range(1,11,1):
+#     print(f"{i} * {num} = {i*num}");
+
+# Question ----------------------------------------4.0)
+
+# num = int(input("Enter the num_"))
+# ans = 0
+# for i in range(0, num+1):
+#     ans += i
+
+# print(ans)
+
+# Question -----------------------------------------5.0) **
+
+# n = int(input("Enter the number -"))
+# fact = 1
+# for i in range(1,n+1):
+#     fact = fact * i
+# print(fact)
+
+# Loops Advance Question's
+
+# Q.6 Print all the factors of a number. -----------------------6.0) ***
+
+# n = int(input("Enter the number_"))
+
+# for i in range(1,n+1):
+#     if n%i == 0:
+#         print(i,end = " ")
+
+# Question----------------------------------------------------------7.0) ***
+
+# n = int(input("Enter your number_"))
+
+# sum = 0
+
+# for i in range(1,n+1):
+#     if n%i == 0:
+#         if i == n:
+#             break
+#         sum += i
+
+# if sum == n:
+#     print("this is a strong num")
+# else:
+#     print("not")
+
+# Check a prime number or not ------------------------------------8.0)**
+
+# n = int(input("Enter a num - "))
+# count = 0
+# for i in range(1,n+1):
+#     if n%i == 0:
+#         count = count + 1
+# if count == 2:
+#     print("This is prime num")
+# else:
+#     print("Composite number")
+
+# n = int(input("Enter the num - "))
+# count = 0
+# for i in range(1 , n+1):
+#     if n%i == 0:
+#         count = count + 1
+# if count == 2:
+#     print("Prime")
+# else:
+#     print("Con")
+
+
+# Q.9) Seprate each digit of a number and print it on the new line -------------------9.0)
+
+
---
+++ Python_Question/main.py
@@ -0,0 +1,62 @@
+#Question -------------------------------------------------------1.0)
+
+# a = int(input("Please enter your frist number : - ")) 
+# b = int(input("Please enter second num : - "))
+# if a > b:
+#     print(f"{a} is greater than {b}")
+# else:
+#     print(f"{b} is greater than {a}")
+
+# Question ----------------------------------------------2.0)
+
+# gender = input("Enter the gender - ")
+
+# if gender == "Male":
+#     print(f"God morning sir")
+# elif gender == "Female":
+#     print(f"Good morning Madam")
+# else:
+#     print("Please type right gender (First letter is capital)")
+
+# Question ------------------------------------------------3.0)
+
+# num = int(input("Enter your number : -"))
+
+# if num%2 == 0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+# Question ---------------------------------------------4.0)
+
+# name = input("Enter your name_")
+# age = int(input("Enter your age _"))
+
+# if age >= 18:
+#     print(f" you can vote")
+# else:
+#     if age < 18 :
+#         print("You'r under 18")
+#     else:
+#         print("Enter valid age")
+
+
+# Question ------------------------------------------5.0)
+
+# year = int(input("Enter the year _"))
+# if year %4 == 0 and year %100 != 0:
+#     print("leap")
+# elif year %100 == 0 and year %400 == 0:
+#     print("leap")
+# else:
+#     print("Not leap")
+
+# Question ------------------------------------------6.0)
+
+# letter = input("Enter the letters_")
+
+# if letter in "aeiouAEIOU":
+#     print("Vowel")
+# else:
+#     print("Cons")
+
