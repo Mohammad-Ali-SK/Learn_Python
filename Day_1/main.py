9ba5a71d14ca5b0bc9a5d9d96ff021b7351dfcfa
Mohammad Ali sk <mohammadalisk050@gmail.com>
2024-02-23 21:23:05 +0530

    2/23/24

--- Day_1/main.py
+++ Day_1/main.py
@@ -79,7 +79,76 @@
 # Logical Operators -----------------------------------------------------7.1)
 # and , or , not
 
-a = 90
-b = 99
+# a = 90
+# b = 99
 # print(a == b and a != b); ------Two element are same
 # print(a == b or a != b); -------Any one element are same
+# print(not True);  --------------Convert True to False and False to True
+
+# Control Flow (if/else) ----------------------------------------------------8.0)
+# simple if statement
+# simple if else statement
+# elif statement
+# nested if else statement
+# pass statement
+# or , and , not operator
+# in, not in keyword
+
+# FOCUS ON CODE PART
+
+
+# Example 1 -------------------------------------------------------------------
+
+# [
+# number = int(input("Enter your number - "))
+# num = 20
+# if number == num:
+#     print("You guessed the right number")
+# else:
+#     if number > num:
+#         print("Your number is very high")
+#     else:
+#         print("Your guess is low")
+
+
+
+#        Example 2--------------------------------------------------------------
+
+# [
+# month = int(input("Enter the number between 1 - 4 : - "))
+# if month == 1:
+#         print("January")
+# elif month == 2:
+#         print("Feb")
+# elif month == 3:
+#         print("March")
+# elif month == 4:
+#         print("Apr")
+# else:
+#         print("You type wrong number") 
+        # ]
+        
+# Example ----------------------------------3
+
+# if 13 > 9 and 340 < 89:
+#         print('You are right')
+# else:
+#         print("You'r wrong")
+
+# Iterative statements(LOOPS) ---------------------------------------------------------9.0)
+
+# Range function
+# For and while
+# Break and continue
+
+
+# For loop in python
+# for i in range(0,10,1):
+#         print(i+1) 
+
+# While loop in python
+
+for i in range(1,11,1):
+        if i == 2 or i == 1 or i == 9:
+                continue
+        print(i);
