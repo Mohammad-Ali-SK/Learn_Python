a83b4d4302a4aa15dddeaf51faa51bb2ecc49275
Mohammad Ali sk <mohammadalisk050@gmail.com>
2024-02-23 18:38:21 +0530

    2/23/24

--- Day_1/main.py
+++ Day_1/main.py
@@ -23,14 +23,63 @@
 # a = '12';
 # a = bool(a);
 # print(type(a)); 
-import pandas as pd
+# import pandas as pd
 
-data = {
-  'Name': ['John', 'Anna', 'Peter', 'Linda'],
-  'Age': [28, 34, 29, 32],
-  'City': ['New York', 'Paris', 'Berlin', 'London']
-}
+# data = {
+#   'Name': ['John', 'Anna', 'Peter', 'Linda'],
+#   'Age': [28, 34, 29, 32],
+#   'City': ['New York', 'Paris', 'Berlin', 'London']
+# }
 
-df = pd.DataFrame(data)
+# df = pd.DataFrame(data)
 
-print(df)
+# print(df)
+
+# a = None
+# # print(type(a));
+# a = bool(a)
+# print(type(a));
+# print(a);
+
+# input() in python -------------------------------------------3.0)
+
+# Now whenever you want to ask something fromm the user you have to use input function.
+
+# input("What is your age ?")
+# print("My name is Akarsh and my age is 22")
+# print(a)
+# input("Enter the number")
+# age = int(input("Enter your age - "));
+# print(type(age));
+# print("My name is Mohammad ALI sk and i \nam 21 yers old")
+
+
+#  Escape character_-----------------------------------------------------------4.0)
+# \', \\, \n, \r, \t, \b, \f  there are many avalable in python 
+# raw string in python
+# print(r"My name is ALi and i am from India \n , \b") ------------Not working escape character in python 
+
+
+# Formating string-----------------------------------------------------------5.0)
+# name = "ALi"
+# age = 22
+# print(f"My name is {name} and i am {age} year old");
+
+
+
+# Arthmetic Operators ------------------------------------------------------6.0)
+# (+, - , /, *, // , ** , %)
+# print(12 + 1999);
+
+
+# Comparison Operators -----------------------------------------------------7.0)
+# (==, !=, > , < , >= , <=)
+# print( 40 != 45)
+
+# Logical Operators -----------------------------------------------------7.1)
+# and , or , not
+
+a = 90
+b = 99
+# print(a == b and a != b); ------Two element are same
+# print(a == b or a != b); -------Any one element are same
