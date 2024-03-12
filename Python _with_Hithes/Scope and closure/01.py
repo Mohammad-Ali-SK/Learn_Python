8471ce21f65a0b73aa829f50b7ee6a60adab3653
Mohammad Ali sk <mohammadalisk050@gmail.com>
2024-03-12 10:37:12 +0530

    12/3/24

---
+++ Python _with_Hithes/Scope and closure/01.py
@@ -0,0 +1,22 @@
+# def name():
+#     x = 10
+#     def name2():
+#         print(x)
+#     return name2
+
+# result = name()
+# result()
+
+# Example ----------------------2
+
+# def mul(x):
+#     def mul2(y):
+#         return x * y
+#     return mul2
+# result = mul(8)
+# print(result(8))
+
+
+# Oops in python by ChaiorCode ------------With Hithes
+
+# Basis class and Object ?
