1b2e98614e19bc102f70c643ab99a17341bdf926
Mohammad Ali sk <mohammadalisk050@gmail.com>
2024-03-14 03:00:09 +0530

    14/3/24

--- Opps_Python/project.py
+++ Opps_Python/project.py
@@ -26,36 +26,60 @@
 # student1.add_marks('Math', 79)
 
 # student1.show_details()
+# class Factory:
+#     def __init__(self,BT,ET,TY):
+#         self.BT = BT
+#         self.ET = ET
+#         self.TY = TY
+
+# ferari = Factory('Covered','8 Cycle', 8)
+
+# print(ferari.TY)
+
+# class Fac:
+#     def __init__(self,bt,et,ty):
+#         self.bt = bt
+#         self.et = et
+#         self.ty = ty
+#     def showDetails(self):
+#         print(f'Your details are  {self.bt} , {self.et}, {self.ty}')
+        
+# class Fac2(Fac):
+#     def __init__(self, bt, et, ty,glass):
+#         super().__init__(bt, et, ty)
+#         self.glass = glass
+#     def showDetails(self):
+#         print(f'Your details are  {self.bt} , {self.et}, {self.ty} , {self.glass}')
 
 
+# ferari = Fac('Covered','8 cycle', 4)
+# alto = Fac2('Covered','8 cycle', 4, 'Iron')
+# ferari.showDetails()
+# alto.showDetails()
 
 class Student:
-    def __init__(self,name,roll,class_no):
+    def __init__(self,name,class_name,roll_num):
         self.name = name
-        self.roll = roll
-        self.class_no = class_no
+        self.class_name = class_name
+        self.roll_num = roll_num
         self.marks = {}
     def add_marks(self,subject,marks):
         if subject in self.marks:
-            print("This this is already declear")
+            print(f'This subject is already exit')
         else:
             self.marks[subject] = marks
-    def cal_avg(self):
+    
+    def calculation_avg(self):
         if not self.marks:
-            print("No Marks is here ")
+            print('There are no subject are here')
         total = sum(self.marks.values())
-        avg = total/len(self.marks)
-        return avg
-    def student_details(self):
-        print(f'Student Information')
-        print(f'Student name is {self.name} and Roll No _{self.roll} Class --{self.class_no}')
-        print(f'{self.marks}')
-        print(f'Avarage Marks is {self.cal_avg()}')
+        avg_marks = total/len(self.marks)
+        return avg_marks
+    
+    def show_detail(self):
+        print('Student Details is__')
+        print(f'roll_{self.roll_num} name_{self.name}  class_{self.class_name} marks = {self.marks}  Avarage marks is _{self.calculation_avg()}')
 
-student1 = Student('Rohit',78,'8th')
+student1 = Student('ALi',8,78)
 student1.add_marks('Math',89)
-student1.add_marks('English',67)
-student1.add_marks('Science',80)
-student1.add_marks('History',56)
- 
-student1.student_details()
+student1.show_detail()
---
+++ Python _with_Hithes/Opps in python/01.py
@@ -0,0 +1,19 @@
+# class Car:
+#     def __init__(self,brand,model):
+#         self.brand = brand
+#         self.model = model
+#     def full_name(self):
+#         print(f'{self.brand} {self.model}')
+
+# class Electric(Car):
+#     def __init__(self, brand, model,BS):
+#         super().__init__(brand, model)
+#         self.BS = BS
+#     def full_name(self):
+#         print(f'{self.brand}, {self.model} and {self.BS}')
+        
+        
+# car_1 = Electric('Tesla' ,'09' , 'Yes')
+# car_2 = Car('Tata', 'Safari')
+# car_1.full_name()
+# car_2.full_name()
