2261ed03132595b5dac7e0f454a4a62d4010458d
Mohammad Ali sk <mohammadalisk050@gmail.com>
2024-03-26 12:04:54 +0530

    26/3/24

---
+++ Function_Python/March_25.py
@@ -0,0 +1,39 @@
+# name = 'I am Mohammad ALi sk'
+# print(name[len(name) - 1])
+
+# a = {1:"Ali", 2:"Rohit", 3:"Suman"}
+# a[4] = 'yash'
+# for i in a:
+#     print(a[i])
+
+
+# a = 'MOhammad'
+
+# for i in range(len(a)-1,-1,-1):
+#     print(a[i])
+
+
+# l = ''
+
+# for i in a:
+#     if i.islower():
+#         l += i
+# for i in a:
+#     if i.isupper():
+#         l += i
+# print(l)
+
+# n = int(input('Enter the num__'))
+# a = []
+
+# for i in range(n):
+#     c = int(input('Enter the number__'))
+#     a.append(c)
+
+# d = []
+# for i in range(len(a)-1, -1, -1):
+#     d.append(a[i])
+# print(d)
+    
+
+
